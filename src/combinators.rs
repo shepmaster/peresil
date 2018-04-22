d8f0673aa2c92406fc777acb8c1508d633b96cb6
Jake Goulding <jake.goulding@gmail.com>
2018-04-22 13:39:54 -0400

    Format the combinators module

--- src/combinators.rs
+++ src/combinators.rs
@@ -1,3 +1,5 @@
+use super::{ParseMaster, Point, Progress, Recoverable, Status};
+
 #[macro_export]
 macro_rules! sequence {
     ($pm:expr, $pt:expr, {let $x:pat = $parser:expr; $($rest:tt)*}, $creator:expr) => {{
@@ -17,14 +19,14 @@ macro_rules! sequence {
     };
 }
 
-use super::{ParseMaster, Progress, Status, Point, Recoverable};
-
 pub trait IntoAppend<T> {
     fn into(self) -> Vec<T>;
 }
 
 impl<T> IntoAppend<T> for Vec<T> {
-    fn into(self) -> Vec<T> { self }
+    fn into(self) -> Vec<T> {
+        self
+    }
 }
 
 impl<T> IntoAppend<T> for Option<T> {
@@ -33,76 +35,90 @@ impl<T> IntoAppend<T> for Option<T> {
     }
 }
 
-pub fn optional<P, E, S, F, T>
-    (parser: F)
-     -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Option<T>, E>
-    where F: FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          P: Point,
-          E: Recoverable,
+pub fn optional<P, E, S, F, T>(
+    parser: F,
+) -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Option<T>, E>
+where
+    F: FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    P: Point,
+    E: Recoverable,
 {
     move |pm, pt| pm.optional(pt, parser)
 }
 
-pub fn optional_append<P, E, S, A, F, T>
-    (append_to: A, parser: F)
-     -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
-    where F: FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          A: IntoAppend<T>,
-          P: Point,
-          //E: Recoverable, // TODO: use this
+pub fn optional_append<P, E, S, A, F, T>(
+    append_to: A,
+    parser: F,
+) -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
+where
+    F: FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    A: IntoAppend<T>,
+    P: Point,
+    //E: Recoverable, // TODO: use this
 {
     move |pm, pt| {
         let mut append_to = append_to.into();
         match parser(pm, pt) {
-            Progress { point, status: Status::Success(v) } => {
+            Progress {
+                point,
+                status: Status::Success(v),
+            } => {
                 append_to.push(v);
                 Progress::success(point, append_to)
             }
-            Progress { point, status: Status::Failure(..) } => {
-                Progress::success(point, append_to)
-            }
+            Progress {
+                point,
+                status: Status::Failure(..),
+            } => Progress::success(point, append_to),
         }
     }
 }
 
-pub fn zero_or_more<P, E, S, F, T>
-    (parser: F)
-     -> impl Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
-    where F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          P: Point,
-          E: Recoverable,
+pub fn zero_or_more<P, E, S, F, T>(
+    parser: F,
+) -> impl Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
+where
+    F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    P: Point,
+    E: Recoverable,
 {
     move |pm, pt| pm.zero_or_more(pt, &parser) // what why ref?
 }
 
-pub fn zero_or_more_append<P, E, S, A, F, T>
-    (append_to: A, parser: F)
-     -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
-    where F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          A: IntoAppend<T>,
-          P: Point,
-          E: Recoverable,
+pub fn zero_or_more_append<P, E, S, A, F, T>(
+    append_to: A,
+    parser: F,
+) -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
+where
+    F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    A: IntoAppend<T>,
+    P: Point,
+    E: Recoverable,
 {
     move |pm, mut pt| {
         let mut append_to = append_to.into();
         loop {
             match parser(pm, pt) {
-                Progress { point, status: Status::Success(v) } => {
+                Progress {
+                    point,
+                    status: Status::Success(v),
+                } => {
                     append_to.push(v);
                     pt = point;
-                },
+                }
                 Progress { .. } => return Progress::success(pt, append_to),
             }
         }
     }
 }
 
-pub fn one_or_more<P, E, S, F, T>
-    (parser: F)
-     -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
-    where F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          P: Point,
-          E: Recoverable,
+pub fn one_or_more<P, E, S, F, T>(
+    parser: F,
+) -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
+where
+    F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    P: Point,
+    E: Recoverable,
 {
     move |pm, pt| {
         let (pt, head) = try_parse!(parser(pm, pt));
@@ -113,13 +129,15 @@ pub fn one_or_more<P, E, S, F, T>
     }
 }
 
-pub fn one_or_more_append<P, E, S, A, F, T>
-    (append_to: A, parser: F)
-     -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
-    where F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          A: IntoAppend<T>,
-          P: Point,
-          E: Recoverable,
+pub fn one_or_more_append<P, E, S, A, F, T>(
+    append_to: A,
+    parser: F,
+) -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, Vec<T>, E>
+where
+    F: Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    A: IntoAppend<T>,
+    P: Point,
+    E: Recoverable,
 {
     move |pm, pt| {
         let mut append_to = append_to.into();
@@ -131,31 +149,29 @@ pub fn one_or_more_append<P, E, S, A, F, T>
     }
 }
 
-pub fn map<P, E, S, F, C, T, U>
-    (parser: F, convert: C)
-     -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, U, E>
-    where F: FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
-          C: FnOnce(T) -> U,
-          P: Point,
-          E: Recoverable,
+pub fn map<P, E, S, F, C, T, U>(
+    parser: F,
+    convert: C,
+) -> impl FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, U, E>
+where
+    F: FnOnce(&mut ParseMaster<P, E, S>, P) -> Progress<P, T, E>,
+    C: FnOnce(T) -> U,
+    P: Point,
+    E: Recoverable,
 {
-    move |pm, pt| {
-        parser(pm, pt).map(convert)
-    }
+    move |pm, pt| parser(pm, pt).map(convert)
 }
 
-pub fn point<P, E, S>
-    (_: &mut ParseMaster<P, E, S>, pt: P)
-     -> Progress<P, P, E>
-    where P: Clone,
+pub fn point<P, E, S>(_: &mut ParseMaster<P, E, S>, pt: P) -> Progress<P, P, E>
+where
+    P: Clone,
 {
     Progress::success(pt.clone(), pt)
 }
 
-pub fn inspect<P, E, S, F>
-    (f: F)
-     -> impl Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, (), E>
-    where F: Fn(&P),
+pub fn inspect<P, E, S, F>(f: F) -> impl Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, (), E>
+where
+    F: Fn(&P),
 {
     move |_, pt| {
         f(&pt);
@@ -163,10 +179,9 @@ pub fn inspect<P, E, S, F>
     }
 }
 
-pub fn state<P, E, S, F>
-    (f: F)
-     -> impl Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, (), E>
-    where F: Fn(&mut S),
+pub fn state<P, E, S, F>(f: F) -> impl Fn(&mut ParseMaster<P, E, S>, P) -> Progress<P, (), E>
+where
+    F: Fn(&mut S),
 {
     move |pm, pt| {
         f(&mut pm.state);
